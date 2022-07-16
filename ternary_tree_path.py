601054437fa6395dc713826076391c7e108c68ca
Thomas Ngo <tngo0508@gmail.com>
2022-07-16 10:57:08 -0700

    ternary tree path

--- ternary_tree_path.py
+++ ternary_tree_path.py
@@ -21,6 +21,40 @@ def ternary_tree_paths(root: Node) -> List[str]:
     helper(root, [str(root.val)], result) 
     return result
 
+# other way to write this
+def ternary_tree_paths(root: Node) -> List[str]:
+    # WRITE YOUR BRILLIANT CODE HERE
+    def dfs(node, curr, res):
+        if node and not node.children:
+            curr.append(str(node.val))
+            res.append("->".join(curr))
+            return
+        
+        for child in node.children:
+            dfs(child, curr + [str(node.val)], res)
+    
+    result = []
+    dfs(root, [], result)
+    return result
+
+# solution
+def ternary_tree_paths(root):
+    # dfs helper function
+    def dfs(root, path, res):
+        # exit condition, reached leaf node, append paths to results
+        if all(c is None for c in root.children):
+            res.append('->'.join(path) + '->' + str(root.val))
+            return
+
+        # dfs on each non-null child
+        for child in root.children:
+            if child is not None:
+                dfs(child, path + [str(root.val)], res)
+
+    res = []
+    if root: dfs(root, [], res)
+    return res
+
 # this function build a tree from input
 # learn more about how trees are encoded in https://algo.monster/problems/serializing_tree
 def build_tree(nodes, f):
