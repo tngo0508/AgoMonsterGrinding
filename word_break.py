b5d2adab3ac542ac4e199102b146e45b4076de93
Thomas Ngo <tngo0508@gmail.com>
2022-07-17 14:13:37 -0700

    word break solution

--- word_break.py
+++ word_break.py
@@ -2,6 +2,11 @@ from typing import List
 
 def word_break(s: str, words: List[str]) -> bool:
     # WRITE YOUR BRILLIANT CODE HERE
+    # native approach
+    # try to combine all the words in word dictionary in any combination
+    # to implement this, use backtracking to try one word at a time, if the length of the constructed string is larger or equal
+    # to the input string. Then, compare to determine if that path is valid or not. 
+
     def backtrack(string, curr, wordDictionary):
         currString = "".join(curr)
         if len(currString) >= len(string):
@@ -23,3 +28,26 @@ def word_break(s: str, words: List[str]) -> bool:
         
     memo = {}
     return backtrack(s, [], words)
+
+    # solution
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        def dfs(start, s):
+            if start >= len(s):
+                return True
+            
+            if start in memo:
+                return memo[start]
+            
+            result = False
+            for word in wordDict:
+                if s[start:].startswith(word):
+                    if dfs(start + len(word), s):
+                        result = True
+                        break
+            
+            memo[start] = result
+            return memo[start]
+        
+        memo = {}
+        return dfs(0, s)
