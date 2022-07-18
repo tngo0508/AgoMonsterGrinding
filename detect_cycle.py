f3d7b0e6611867a097c031c3e63055a974dd475a
Thomas Ngo <tngo0508@gmail.com>
2022-07-18 11:44:35 -0700

    detect cycle

---
+++ detect_cycle.py
@@ -0,0 +1,84 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        slow = head
+        fast = head.next
+        while fast and fast.next:
+            if fast is slow:
+                break
+            slow = slow.next
+            fast = fast.next.next
+            
+        if fast is None:
+            return fast
+            
+        sentinel = ListNode(-1)
+        sentinel.next = head
+        slow = sentinel
+        while fast and slow is not fast:
+            slow = slow.next
+            fast = fast.next
+            
+        return fast
+
+
+# leetcode solution
+# using hashmap
+class Solution(object):
+    def detectCycle(self, head):
+        visited = set()
+
+        node = head
+        while node is not None:
+            if node in visited:
+                return node
+            else:
+                visited.add(node)
+                node = node.next
+
+        return None
+
+# hare and tortoise
+class Solution(object):
+    def getIntersect(self, head):
+        tortoise = head
+        hare = head
+
+        # A fast pointer will either loop around a cycle and meet the slow
+        # pointer or reach the `null` at the end of a non-cyclic list.
+        while hare is not None and hare.next is not None:
+            tortoise = tortoise.next
+            hare = hare.next.next
+            if tortoise == hare:
+                return tortoise
+
+        return None
+
+    def detectCycle(self, head):
+        if head is None:
+            return None
+
+        # If there is a cycle, the fast/slow pointers will intersect at some
+        # node. Otherwise, there is no cycle, so we cannot find an entrance to
+        # a cycle.
+        intersect = self.getIntersect(head)
+        if intersect is None:
+            return None
+
+        # To find the entrance to the cycle, we have two pointers traverse at
+        # the same speed -- one from the front of the list, and the other from
+        # the point of intersection.
+        ptr1 = head
+        ptr2 = intersect
+        while ptr1 != ptr2:
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next
+
+        return ptr1
