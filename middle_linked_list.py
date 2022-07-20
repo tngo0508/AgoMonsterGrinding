66bf4be757d8241ee7f488c20add34da4edc12e9
Thomas Ngo <tngo0508@gmail.com>
2022-07-20 10:01:29 -0700

    middle linked list

---
+++ middle_linked_list.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+def middle_of_linked_list(head: Node) -> int:
+    # WRITE YOUR BRILLIANT CODE HERE
+    sentinel = Node(-1)
+    sentinel.next = head
+    slow = sentinel
+    fast = sentinel.next
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    
+    return slow.next.val if slow and slow.next else slow
