90dba80bcc75ca2ed7da25890dce57a7ecfa7a22
Thomas Ngo <tngo0508@gmail.com>
2022-07-28 16:46:08 -0700

    my solution for diagonal problem

--- diagonal_traversal.js
+++ diagonal_traversal.js
@@ -1,6 +1,24 @@
 /**
  * @param {number[][]} mat
  * @return {number[]}
+ * the idea is to know when to determine how to move diagonal up right or bottom left
+ * to move up and right, we just need to do row - 1 and col + 1
+ * to move bottom and left, we do row + 1 and col - 1
+ * when the index is out of bound, we need to reset the index for the next iteration properly
+ * for example, 
+ * 
+ *     #   #
+ *   1 2 3
+ *   4 5 6
+ * # 7 8 9 
+ *   #
+ * 
+ *  the # is where we reach the out of bound position, so
+ *  if we are moving in the direction up and right:
+ *      we check for col first, if it's out of bound, we move the row down
+ *      otherwise, we check for row, if row is out of bound, just move the col to the right
+ *  if we are moving in the direction bottom and left:
+ *      do the same thing, but check the col first, then row
  */
  var findDiagonalOrder = function(mat) {
     let row = 0, col = 0;
