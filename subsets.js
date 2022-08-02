0c5d695f4365cbe0f377d3d2041a0f1f5f4eb833
Thomas Ngo <tngo0508@gmail.com>
2022-08-02 10:14:15 -0700

    review subsets

---
+++ subsets.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+// trick is to use i + 1 to pass as the argument instead of start or start + 1
+// review the permutation and combination sum to compare the ways how to traverse the tree path while backtracking
+ var subsets = function(nums) {
+    function backtrack(start, curr, res, nums) {
+        if (start > nums.length) return;
+        res.push([...curr]);
+        for (let i = start; i < nums.length; ++i) {
+            curr.push(nums[i]);
+            backtrack(i + 1, curr, res, nums);
+            curr.pop();
+        }
+    }
+    const res = [];
+    backtrack(0, [], res, nums);
+    return res;
+};
+
+// algomonster solution
+// idea: include and don't include
+function subsets(nums) {
+    const res = [];
+    function dfs(i, cur) {
+        if (i == nums.length) {
+            res.push([...cur]);
+            return;
+        }
+        cur.push(nums[i]);
+        dfs(i + 1, cur);
+        cur.pop();
+        dfs(i + 1, cur);
+    }
+    dfs(0, []);
+    return res;
+}
