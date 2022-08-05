b87e9068a3f6f1c21ab0b0913a0e4bb229099c7a
Thomas Ngo <tngo0508@gmail.com>
2022-08-05 15:41:46 -0700

    review disjoin set

--- find_path_exists_in_graph.js
+++ find_path_exists_in_graph.js
@@ -5,7 +5,37 @@
  * @param {number} destination
  * @return {boolean}
  */
- var validPath = function(n, edges, source, destination) {
+
+ class UnionFind {
+    constructor(size) {
+      this.root = Array.from({length: size}, (_, i) => i);
+      this.rank = new Array(size).fill(1);
+    }
+    
+    find(x) {
+      if (x === this.root[x]) return x;
+      this.root[x] = this.find(this.root[x]);
+      return this.root[x];
+    }
+    
+    union(x, y) {
+      let rootX = this.find(x);
+      let rootY = this.find(y);
+      if (rootX != rootY) {
+        if (this.rank[rootX] > this.rank[rootY]) this.root[rootY] = rootX;
+        else if (this.rank[rootX] < this.rank[rootY]) this.root[rootX] = rootY;
+        else {
+          this.root[rootY] = rootX;
+          this.rank[rootX] += 1;
+        }
+      }
+    }
+    
+    connected(x, y) {
+      return this.find(x) === this.find(y);
+    }
+  }
+  var validPath = function(n, edges, source, destination) {
     function dfs(src, dst, graph, visited) { // TLE - time limit exceeded
       if (src === dst) return true;
       for (const neighbor of graph.get(src)) {
@@ -33,6 +63,7 @@
       return false;
     }
     
+    let union = new UnionFind(n);
     const graph = new Map();
     for (let i = 0; i < n; ++i) {
       graph.set(i, []);
@@ -40,8 +71,10 @@
     for (const edge of edges) {
       graph.get(edge[0]).push(edge[1]);
       graph.get(edge[1]).push(edge[0]);
+      union.union(edge[0], edge[1]);
     }
     const visited = new Array(n).fill(false);
     visited[source] = true;
-    return bfs(source, destination, graph, visited);
+    // return bfs(source, destination, graph, visited);
+    return union.connected(source, destination);
   };
