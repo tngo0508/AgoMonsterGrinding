9d98099cddf49a09278653f1618ebfe689c0c411
Thomas Ngo <tngo0508@gmail.com>
2022-08-08 09:20:53 -0700

    review dfs

---
+++ path_sum.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+ var hasPathSum = function(root, targetSum) {
+    function dfs(node, target) { 
+      if (!node) return false;
+      
+      target -= node.val;
+      
+      if (!node.left && !node.right) {
+        return target === 0;
+      }
+      
+      let l = dfs(node.left, target);
+      let r = dfs(node.right, target);
+      return l || r;
+    }
+    
+    return dfs(root, targetSum);
+  };
