32a45fb285a277e309afb35fb1c7f2009ca7fd1e
Thomas Ngo <tngo0508@gmail.com>
2022-08-07 17:51:23 -0700

    review detect cycle in linked list

---
+++ cycle_finding.js
@@ -0,0 +1,33 @@
+class Node {
+    constructor(val, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+function hasCycle(nodes) {
+    // WRITE YOUR BRILLIANT CODE HERE
+    let slow = nodes, fast = nodes;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) return true;
+    }
+    return false;
+}
+
+// solution
+function nextNode(node) {
+    if (node.next == null) return node;
+    return node.next;
+}
+
+function hasCycle(nodes) {
+    let tortoise = nextNode(nodes);
+    let hare = nextNode(nextNode(nodes));
+    while (tortoise !== hare && hare.next != null) {
+        tortoise = nextNode(tortoise);
+        hare = nextNode(nextNode(hare));
+    }
+    return hare.next != null;
+}
